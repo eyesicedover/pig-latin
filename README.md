2afb487a8a7ef56f7d56a1565d5d28d4a5e506a8
Kaila Goff and Stephanie Faber <stephanie@>
2018-01-18 14:52:19 -0800

    add y as a consonant spec

---
+++ README.md
@@ -0,0 +1,19 @@
+# Pig Latin
+
+## Kaila Goff and Stephanie Faber version 1.0, 1/18/2018
+
+### A website to translate sentences into pig latin.
+
+#### no known bugs at this time
+
+# Setup/Installation
+###### _clone from GitHub_
+###### _<a href="https://kailagoff.github.io/pig-latin/">Program Language</a>_
+
+## Technologies used
+
+_HTML, CSS, JavaScript, Bootstrap, & jQuery_
+
+### This project is under the MIT License
+
+### Copyright 2018 _Kaila Goff_ & _Stephanie Faber_
--- js/scripts.js
+++ js/scripts.js
@@ -3,44 +3,69 @@ $(document).ready(function(){
     event.preventDefault();
 
     var sentence = $("input#sentence").val();
-    var vowels = ["a", "e", "i", "o", "u"];
+    var vowels = ["a", "e", "i", "o", "u", "y"];
     var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     var result = "";
     var consonant = "";
     var extract = "";
-
+    var breakCheck = false;
+    debugger;
     for (var h = 0; h < numbers.length; h++) {
       if (sentence.charAt(0) ===  numbers[h]) {
         result = sentence;
         break;
-      } else {
+      } else if ((sentence.startsWith("y") === true) || (sentence.startsWith("Y") === true)) {
+          for (var l = 0; l < sentence.length; l++) {
+            for (var k = 0; k < (vowels.length - 1); k++) {
+              if (sentence.charAt(l) === vowels[k]) {
+                var vowelIndex = l;
+                breakCheck = true;
+                break;
+              } else {
+                continue;
+              }
+            }
+            if (breakCheck === true) {
+              consonant = sentence.slice(0, vowelIndex);
+              extract = sentence.slice(vowelIndex);
+              result = extract.concat(consonant, "ay");
+              break;
+            }
+          } break;
+        } else {
         for (var i = 0; i < vowels.length; i++) {
           if (sentence.charAt(0) === vowels[i]) {
             result = sentence.concat("way");
+            break;
           } else {
-            consonant = sentence.charAt(0);
-            extract = sentence.slice(1);
+            for (var l = 0; l < sentence.length; l++) {
+          	  for (var k = 0; k < vowels.length; k++) {
+          		  if (sentence.charAt(l) === vowels[k]) {
+              	  var vowelIndex = l;
+                  breakCheck = true;
+                  break;
+                } else {
+                  continue;
+                }
+              }
+              if (breakCheck === true) {
+                break;
+              }
+            }
+            consonant = sentence.slice(0, vowelIndex);
+            extract = sentence.slice(vowelIndex);
             result = extract.concat(consonant, "ay");
+
+            if ((consonant.endsWith("q") === true) && (extract.startsWith("u") === true)) {
+              vowelIndex += 1;
+              consonant = sentence.slice(0, vowelIndex);
+              extract = sentence.slice(vowelIndex);
+              result = extract.concat(consonant, "ay");
+            }
           }
         }
       }
     }
-    debugger;
-    for (var l = 0; l < sentence.length; l++) {
-  	  for (var k=0; k <vowels.length; k++) {
-  		  if (sentence.charAt(l) === vowels[k]) {
-      	  var sentenceIndex = l;
-          var breakCheck = true;
-          debugger;
-          break;
-        } else {
-          continue;
-        }
-      }
-      if (breakCheck === true) {break;}
-    }
-
-    alert(sentenceIndex);
 
     $(".output").text(result);
   });
